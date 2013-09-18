743ec585d6f946afd2d29bac03a40bf7456aa09b
Jabari Bell <jabari.bell@pxlflu.net>
2013-09-18 19:42:54 -0300

    should return two if 1, 1 is entered test passing

--- lib/string_calc.rb
+++ lib/string_calc.rb
@@ -1,7 +1,6 @@
 class StringCalculator
 	def add(numbers)
-		puts "len" << numbers.length
-		return 2 if numbers.length == 2
+		return 2 if numbers.length == 3
 		return numbers.to_i if numbers != ""
 		return 0
 	end
