125251107d896fcd8301b53d3feac9e15738b327
Jabari Bell <jabari.bell@pxlflu.net>
2013-09-20 20:39:07 -0300

    refactored and added make_calc method

--- spec/string_calc_spec.rb
+++ spec/string_calc_spec.rb
@@ -1,19 +1,24 @@
 require_relative "../lib/string_calc"
 
 describe "StringCalculator" do
+
+	def make_calc
+		StringCalculator.new
+	end
+
 	context "Addition" do 
 		it "should return zero when empty string is entered" do
-			calc = StringCalculator.new.add('')
+			calc = make_calc.add('')
 			calc.should eql 0
 		end
 		
 		it "should return 1 if 1 is entered" do 
-			calc = StringCalculator.new.add('1')
+			calc = make_calc.add('1')
 			calc.should eql 1
 		end
 
 		it "should return 2 if 2 is entered" do 
-			calc = StringCalculator.new.add('2')
+			calc = make_calc.add('2')
 			calc.should eql 2
 		end
 	end
