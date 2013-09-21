773c42d48101621d817f820165bc18c8a90341d9
Jabari Bell <jabari.bell@pxlflu.net>
2013-09-21 13:33:33 -0300

    arbitrary delimiter test passing

--- lib/string_calc.rb
+++ lib/string_calc.rb
@@ -2,7 +2,10 @@ class StringCalculator
 	def add(numbers)
 		sum = 0
 		return sum if numbers.empty?
-		numbers.split( "," ).each do |c| 	
+		delimiter = ","
+		delimiter = numbers[2] if numbers[0] == "/" && numbers[1] == "/"
+		numbers = numbers[4..-1] if numbers[0] == "/" && numbers[1] == "/"
+		numbers.split( delimiter ).each do |c| 	
 			c.split( "\n" ).each do |sc|
 				sum += sc.to_i 
 			end
