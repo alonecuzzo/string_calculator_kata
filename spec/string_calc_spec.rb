beaacbbf6833d45caa1290ec0d91e4a96c627ca8
Jabari Bell <jabari.bell@pxlflu.net>
2013-09-21 13:07:19 -0300

    single number returning test passing

--- spec/string_calc_spec.rb
+++ spec/string_calc_spec.rb
@@ -6,5 +6,9 @@ describe "StringCalculator" do
 			calc = StringCalculator.new.add("")
 			calc.should eql 0
 		end
+		it "should return 1 when 1 is entered" do
+			calc = StringCalculator.new.add("1")
+			calc.should eql 1
+		end
 	end
 end
